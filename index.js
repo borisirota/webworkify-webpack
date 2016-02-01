5204684896c053ec59ef9a4ade37a581d532f0a8
Boris Sirota <borisirota@gmail.com>
2016-02-01 18:02:25 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,50 @@
+var __webpack_require__ = arguments[2];
+var sources = __webpack_require__.m;
+
+var webpackBootstrapFunc = function(modules) {
+    var installedModules = {};
+    function __webpack_require__(moduleId) {
+      if(installedModules[moduleId])
+        return installedModules[moduleId].exports;
+      var module = installedModules[moduleId] = {
+        exports: {},
+        id: moduleId,
+        loaded: false
+      };
+      modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+      module.loaded = true;
+      return module.exports;
+    }
+    __webpack_require__.m = modules;
+    __webpack_require__.c = installedModules;
+    __webpack_require__.p = "js/";
+    return __webpack_require__(entryModule);
+}
+
+module.exports = function (fn) {
+    var key;
+    for (var i = 0, l = sources.length; i < l; i++) {
+        var exp = __webpack_require__(i);
+        // Using babel as a transpiler to use esmodule, the export will always
+        // be an object with the default export as a property of it. To ensure
+        // the existing api and babel esmodule exports are both supported we
+        // check for both
+        if (exp === fn || exp.default === fn) {
+            key = i;
+            break;
+        }
+    }
+
+    var src = '(' + webpackBootstrapFunc.toString().replace('entryModule', key) + ')(['
+        + sources.map(function (func) {
+            return func.toString();
+        }).join(',')
+        + '])();'
+    ;
+
+    var URL = window.URL || window.webkitURL || window.mozURL || window.msURL;
+
+    return new Worker(URL.createObjectURL(
+        new Blob([src], { type: 'text/javascript' })
+    ));
+};
