d2fd2f4e0e6f6b9138eaf248e199490e732ebde0
Taylor Everding <Taylor@Everding.email>
2016-05-21 15:48:34 -0600

    Extend example suite

--- example/main.js
+++ example/main.js
@@ -1,8 +1,19 @@
+var commonWorker = require('./common-js-worker.js');
+import es6Worker from './es6-worker.js';
+
 var work = require('../');
 
-var w = work(require('./worker.js'));
-w.addEventListener('message', function (ev) {
-    console.log(ev.data);
+
+var w1 = work(commonWorker);
+w1.addEventListener('message', function (ev) {
+    console.log('CommonJS Worker:', ev.data);
 });
 
-w.postMessage(4); // send the worker a message
+w1.postMessage(4); // send the worker a message
+
+var w2 = work(es6Worker);
+w2.addEventListener('message', function (ev) {
+    console.log('ES6 Worker', ev.data);
+});
+
+w2.postMessage(4); // send the worker a message
