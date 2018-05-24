2b701761b19251214ff26b807c4adedc8ec48db2
zuchao.wang <zuchao.wang@gmail.com>
2018-05-24 20:56:14 +0800

    force module to be integer

--- index.js
+++ index.js
@@ -109,6 +109,15 @@ function getModuleDependencies (sources, module, queueName) {
     retval[match[2]].push(match[4])
   }
 
+  // force module to be integer, this can be important after uglify-js converted 1000 to 1e3
+  // in such case need to convert 1e3 back to 1000
+  var keys = Object.keys(retVal);
+  for (var i = 0; i < keys.length; i++) {
+    for (var j = 0; j < retVal[keys[i]].length; i++) {
+      retVal[keys[i]][j] = +retVal[keys[i]][j];
+    }
+  }
+
   return retval
 }
 
